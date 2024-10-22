1d0d41a0fcb279cdeebd2a319f53c4425e4cd650
Basilogast <duyhung08112003@gmail.com>
2024-10-22 16:36:23 +0700

    update sanity 1

--- sanity/schemaTypes/blog.js
+++ sanity/schemaTypes/blog.js
@@ -1,51 +1,67 @@
 export default {
-    name: 'blog',
-    type: 'document',
-    title: 'Blog',
-    fields: [
-      {
-        name: 'title',
-        type: 'string',
-        title: 'Title of blog article',
+  name: 'blog',
+  type: 'document',
+  title: 'Blog',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title of blog article',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug of your blog article',
+      options: {
+        source: 'title',
+        maxLength: 200,
       },
-      {
-        name: 'slug',
-        type: 'slug',
-        title: 'Slug of your blog article',
-        options: {
-          source: 'title',
-          maxLength: 200,
+    },
+    {
+      name: 'titleImage',
+      type: 'image',
+      title: 'Title Image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'smallDescription',
+      type: 'text',
+      title: 'Small Description',
+    },
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content Blocks',
+      of: [
+        {
+          type: 'block',
         },
-      },
-      {
-        name: 'titleImage',
-        type: 'image',
-        title: 'Title Image',
-        options: {
-          hotspot: true,
+        {
+          type: 'image',
+          title: 'Image',
+          options: {
+            hotspot: true,
+          },
         },
-      },
-      {
-        name: 'smallDescription',
-        type: 'text',
-        title: 'Small Description',
-      },
-      {
-        name: 'content',
-        type: 'array',
-        title: 'Content Blocks',
-        of: [
-          {
-            type: 'block',
+        {
+          type: 'file',
+          name: 'pdfFile',
+          title: 'PDF File',
+          options: {
+            accept: '.pdf',
           },
-          {
-            type: 'image',
-            options: {
-              hotspot: true,
-            },
+        },
+        {
+          type: 'file',
+          name: 'videoFile',
+          title: 'Video File',
+          options: {
+            accept: 'video/*', // Allows all video formats
           },
-        ],
-      },
-    ],
-  };
-  
+        },
+      ],
+    },
+  ],
+};
